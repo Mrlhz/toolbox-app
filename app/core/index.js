431787c577762b67176ea268fc85ef1217a29cc1
mrlhz <mr__lhz@163.com>
2022-05-23 05:14:42 +0800

    remove yargs related functions

--- app/core/index.js
+++ app/core/index.js
@@ -1,12 +1,7 @@
 const clearAdsFiles = require('./clearAdsFiles')
 const { flatDirectory } = require('./flatDirectory')
 
-const initClearAdsFile = require('.//initClearAdsFile')
-const initFlat = require('./initFlat')
-
 module.exports = {
   clearAdsFiles,
-  flatDirectory,
-  initClearAdsFile,
-  initFlat
+  flatDirectory
 }
